40fe8423b4786b7b4945acf43a440bc746bddaf2
Charan <139607786+TeamKanyarasi@users.noreply.github.com>
2024-04-13 19:42:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ provider "aws" {
 resource "aws_instance" "webserver1" {
   ami                    = "ami-007020fd9c84e18c7"
   instance_type          = "t2.micro"
-  vpc_security_group_ids = [sg-00e239015e45bcb80]
+  vpc_security_group_ids = ["sg-00e239015e45bcb80"]
   subnet_id              = "subnet-0713506a0f78c0d52"
   key_name               = "test-st"           #var.key_name
   user_data              = base64encode(file("userdata.sh"))
